766d35697e3098042a58bd2a1bfe964ef2344dc6
NickyEXE <dovern42@gmail.com>
2020-12-01 14:19:52 -0500

    adds Yelp Search capability

---
+++ app/models/yelp_search.rb
@@ -0,0 +1,18 @@
+class YelpSearch
+
+  attr_reader :results
+
+  def initialize(location)
+    url = "https://api.yelp.com/v3/businesses/search"
+    params = {
+      term: "lunch",
+      location: location,
+      limit: 50
+    }
+
+    response = HTTP.auth("Bearer #{ENV['YELP_KEY']}").get(url, params: params)
+    @results = response.parse
+  end
+
+
+end
